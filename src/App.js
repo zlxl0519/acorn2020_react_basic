1fb15e632d8262fbb201354419ef8858c46e555e
gura <oli999@naver.com>
2020-09-18 10:35:50 +0900

    App 함수표현식을 class 표현식으로 변경함

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React,{ Component } from 'react';
 //이미지 경로를 import 해서 사용한다.
 import logo from './logo.svg';
 //css 파일을 import 만 해도 css 가 자동 적용된다.
 import './App.css';
 
-function App() {
-  const myName="김구라";
-  return (
-    <div>
-      <h1>Hello React!</h1>
-      <p>내이름은 <strong>{myName}</strong></p>
-      <img src={logo} alt="react js 로고 입니다."/>
-    </div>
-  );
+//class 표현식으로 App 구성하기 
+class App extends Component {
+  render() {
+    const myName="김구라";
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>내이름은 <strong>{myName}</strong></p>
+        <img src={logo} alt="react js 로고 입니다."/>
+      </div>
+    );
+  }
 }
 
 export default App;

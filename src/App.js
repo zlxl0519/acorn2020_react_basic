f46fa344b2929a6fe77bbe1e3b36e11a967edbed
seunghui <zlxl0519@naver.com>
2020-09-18 10:20:25 +0900

    App.js App.css 수정함

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+//이미지 경로를 import 해서 사용한다.
 import logo from './logo.svg';
+//css 파일을 import 만 해도 css 가 자동 적용된다.
 import './App.css';
 
 function App() {
+  const myName="김구라";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React!</h1>
+      <p>내이름은 <strong>{myName}</strong></p>
+      <img src={logo} alt="react js 로고 입니다."/>
     </div>
   );
 }

482b2be9495dce9fec1b61ede205cc0c28053514
gura <oli999@naver.com>
2020-09-21 12:45:40 +0900

    App03_example.js 수정함

--- src/App03_example.js
+++ src/App03_example.js
@@ -20,12 +20,29 @@ class App03_example extends Component {
             this.setState({msgs:result});
         }
     }
-
+    //전송 버튼을 눌럿을때 호출되는 함수 
+    handleClick=()=>{
+        //입력한 문자열
+        let msg=this.inputText.value;
+        let content=(<li key={this.state.index}>{msg}</li>);
+            this.setState({index: this.state.index+1});
+        //기존의 배열에 아이템을 추가해서 새로운 배열을 만들어내기
+        let result=this.state.msgs.concat(content);
+        //새로운 배열을 state 에 적용해서 UI 가 수정되도록 한다.
+        this.setState({msgs:result});
+    }
     render() {
         return (
             <div>
                 <h1>목록 출력 예제 입니다.</h1>
+                <input ref={
+                    //함수의 인자로 input 요소의 참조값이 전달된다.
+                    //전달된 참조값을 필드에 inputText 라는 키값으로 저장한다.
+                    (ref)=>{this.inputText=ref}
+                } type="text" onKeyUp={this.handleKeyUp}/>
                 <input type="text" onKeyUp={this.handleKeyUp}/>
+                <input type="text" onKeyUp={this.handleKeyUp}/>
+                <button onClick={this.handleClick}>전송</button>
                 <ul>{this.state.msgs}</ul>
             </div>
         );

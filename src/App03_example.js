6437b16c8ee9b0b31b62d5d5913cf811e40caa42
seunghui <zlxl0519@naver.com>
2020-09-21 12:16:19 +0900

    App3예제 추가(input 에 입력후 엔터 누르면 배열이 추가되면서 key 값을가지고적용

---
+++ src/App03_example.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App03_example extends Component {
+    state={
+        index:0,
+        msgs:[]
+    };
+
+    handleKeyup=(e)=>{
+        //눌러진 키의코드값
+        console.log(e.keyCode);
+        //input 요소에 입력한 value 값
+        console.log(e.target.value);
+        if(e.keyCode == 13){//엔터키를 눌렀을때
+            let content=(<li key={this.state.index}>{e.target.value}</li>);
+            this.setState({index: this.state.index+1});
+            //기존의 배열에 아이템을 추가해서 새로운 배열을 만들어내기
+            let result=this.state.msgs.concat(content);
+            //새로운 배열을 state 에 적용해서 UI 가 수정되도록 한다.
+            this.setState({msgs:result});
+        }
+    }
+
+    render() {
+        return (
+            <div>
+               <h1>목록 출력 예제입니다.</h1>
+               <input type="text" onKeyUp={this.handleKeyup}/>
+                <ul>{this.state.msgs}</ul> 
+            </div>
+        );
+    }
+}
+
+export default App03_example;

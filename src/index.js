b884557dfe422882ec6bbdfa50bb4f4245a971ce
gura <oli999@naver.com>
2020-09-21 12:14:42 +0900

    App03.js 추가함

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App02';
+import App from './App03_example';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(

14c9a78068c86a23e023fb9de60cab51dd471716
seunghui <zlxl0519@naver.com>
2020-09-23 11:04:02 +0900

    App03_example4(입력하는 것을 따로 컴포넌트를 만들어서 이용)

---
+++ src/sub/InputComponent.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+
+class InputComponent extends Component {
+    render() {
+        return (
+            //jsx 를 감싸주는 Fragmemt div 가 필요 없을때 사용
+            <Fragment>
+               <input ref={(ref)=>{this.inputName=ref}} type="text" placeholder="이름 입력..."/>
+                <input ref={(ref)=>{this.inputAddr=ref}} type="text" placeholder="주소 입력..."/>
+                <button onClick={()=>{
+                    const name=this.inputName.value;
+                    const addr=this.inputAddr.value;
+                    //부모 컴포넌트에 전달
+                    this.props.addClicked(name, addr);
+                }}>추가</button> 
+            </Fragment>
+        );
+    }
+}
+
+export default InputComponent;

da4066fc3a29cd5813bf07d534f0e3927c627899
seunghui <zlxl0519@naver.com>
2020-09-23 12:07:03 +0900

    App04_css.js 추가(특정 컴포넌트에서만 css를 적용하는 방법)

---
+++ src/App04_css.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+/*
+    [모듈화된 css를 사용하는 방법]
+    1. css 파일의 이름에 .module. 을 추가 한다.
+    2. from 과 함께 import 해서 사용한다.
+*/
+import styles from './my_css/app04_custom.module.css';
+import MyComponent from './sub/MyComponent';
+
+class App04_css extends Component {
+    render() {
+        return (
+            <div>
+                <h1>react 에서 css 다루기</h1>
+                <div className={styles.box}>app04 box</div>
+                <MyComponent/>
+            </div>
+        );
+    }
+}
+
+export default App04_css;

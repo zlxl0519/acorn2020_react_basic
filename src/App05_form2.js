4aa5b3bd39940f1425051b14bcea37d80eeea4c2
seunghui <zlxl0519@naver.com>
2020-09-25 10:59:10 +0900

    App05_form2,3(form 데이터 출력방법, 오브젝트 방 만드는법)

--- src/App05_form2.js
+++ src/App05_form2.js
@@ -4,20 +4,25 @@ import './css/bootstrap.css';
 class App05_form2 extends Component {
     state={
         id:'',
-        pwd:''
+        pwd:'',
+        isSave:false //첵크박스의 체크 여부
     }
+
     //input 요소에 변화가 생겼을때 호출되는 함수
-    handleChange=(e)=>{
-        //input 요소에 적은 name 값을 가져온다.
-        const name=e.target.name;//"pwd" 가정
+    handleChange=(e)=>{ 
+        //이벤트가 일어난 폼요소의 name 속성의 value 얻어오기
+        const name=e.target.name;//"pwd" 가정 //"id" or "pwd"or "isSave"
+        // value 를 미리 얻어내기 (체크박스인 경우에는 체크 여부를 얻어낸다. 체크박스인경우에는 체크여부 아니면 value 값을 얻어낸다.)
+        const value=e.target.type === 'checkbox' ? e.target.checked : e.target.value;
         /*
         const obj={};
         obj[name]=e.target.value; //obj[pwd] pwd 방을 만들어서 value 값을 넣어주는것//1234
         this.setState(obj);//ogj는 {pwd:"1234"} 가 된다.
         */
+       //ECMA6 문법을 활용하면 아래와 같이 된다.
        this.setState({
            // let a="pwd" {[a]:"1234"} //{pwd:"1234"}
-           [name]:e.target.value
+           [name]:value 
         });
     }
     //폼에 submit 이벤트가 발행했을때 호출되는 함수
@@ -37,11 +42,17 @@ class App05_form2 extends Component {
                         <label htmlFor="pwd">비밀번호</label>
                         <input type="text" value={this.state.pwd} onChange={this.handleChange} className="form-control" name="pwd" id="pwd"/>
                     </div>
-
+                    <div className="form-group">
+                        <label>
+                            <input name="isSave" onChange={this.handleChange} checked={this.state.isSave} type="checkbox" />아이디 저장
+                        </label>
+                    </div>
                     <button className="btn btn-primary" type="submit">로그인</button>
                 </form>
                 <p> 아이디: <strong>{this.state.id}</strong></p>
                 <p> 비밀번호: <strong>{this.state.pwd}</strong></p>
+                {/* boolean type 은 그냥 출력해주지 않으므로 String type 으로 변환시켜서 나오게 한다. */}
+                <p> 아이디 저장할지 여부 : <strong>{this.state.isSave.toString()}</strong></p>
             </div>
         );
     }

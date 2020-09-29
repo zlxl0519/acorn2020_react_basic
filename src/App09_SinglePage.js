05b4e09a13117f0ad3791027522e80382c8fc770
seunghui <zlxl0519@naver.com>
2020-09-29 10:51:46 +0900

    App09_SinglePage 구현중...

--- src/App09_SinglePage.js
+++ src/App09_SinglePage.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route, withRouter } from 'react-router-dom';
 import { Buy, Game, Home, Study } from './pages';
 
+// 하단에서 withRouter() 를 활용해서 export 했기 때문에
+// location, match, hisroty props 가 전달된다.
+const App09_SinglePage=({location, macth, history})=>{
+    
+    const activeStyle={
+        color:"red",
+        fontWeight:"bold"
+    };
 
-const App09_SinglePage=()=>{
+    const [path, setPath]=React.useState('');
+
+    //input 요소에 문자열을 입력 했을때 경로를 바꿔주고
+    const changed=(e)=>{
+        setPath(e.target.value);
+    };
+    //이동 버튼을 눌렀을때 해당 경로로 이동 시킨다.
+    const move=()=>{
+        console.log(path);
+        //history 객체를 이용해서 강제 이동 시킨다.
+        history.push(path);
+    };
+
     return(
         <div>
             <h1>Router 테스트</h1>
+            <input onChange={changed} value={path} type="text" placeholder="이동할경로 입력"/>
+            <button onClick={move}>이동</button>
             <ul>
                 {/* 다른 링크 페이지로 이동 */}
                 {/* <li><a href="/">home</a></li>
@@ -14,17 +36,17 @@ const App09_SinglePage=()=>{
                 <li><a href="/game">geme</a></li> */}
                 
                 {/* 같은 페이지 내에서의 이동 */}
-                <li><Link to="/">home</Link></li>
-                <li><Link to="/study">study</Link></li>
-                <li><Link to="/game">game</Link></li>
+                <li><NavLink to="/" activeStyle={activeStyle}>home</NavLink></li>
+                <li><NavLink to="/study" activeStyle={activeStyle}>study</NavLink></li>
+                <li><NavLink to="/game" activeStyle={activeStyle}>game</NavLink></li>
                 
                 {/* <Route path="/game/:name" component={Game}/> 로 처리된다. */}
-                <li><Link to="/game/star">star game</Link></li>
-                <li><Link to="/game/cart">cart game</Link></li>
+                <li><NavLink to="/game/star" activeStyle={activeStyle}>star game</NavLink></li>
+                <li><NavLink to="/game/cart" activeStyle={activeStyle}>cart game</NavLink></li>
 
-                <li><Link to="/buy?num=1&name=handphone">1 번 상품 사기</Link></li>
-                <li><Link to="/buy?num=2&name=computer">2 번 상품 사기</Link></li>
-                <li><Link to="/buy?num=3&name=coffee">3 번 상품 사기</Link></li>
+                <li><NavLink to="/buy?num=1&name=handphone" activeStyle={activeStyle}>1 번 상품 사기</NavLink></li>
+                <li><NavLink to="/buy?num=2&name=computer" activeStyle={activeStyle}>2 번 상품 사기</NavLink></li>
+                <li><NavLink to="/buy?num=3&name=coffee" activeStyle={activeStyle}>3 번 상품 사기</NavLink></li>
             </ul>
             {/* Home 이 정확히 일치할때만 나오게 하겠다 exact path 는뜻 */}
             <Route exact path="/" component={Home}/>
@@ -35,5 +57,6 @@ const App09_SinglePage=()=>{
         </div>
     );
 };
-
-export default App09_SinglePage;
+//withRouter() 함수를 호출해서 export 해주면 Router 에 관련된
+// props 가 이 컴포넌트에 전달된다.
+export default withRouter(App09_SinglePage);
